f657be84a869c745af8da7a596a1c6895d2341c7
Renata Carriero <Renata.Carriero@icubed.it>
2022-05-16 15:53:54 +0200

    Aggiunta Esercitazione 1

---
+++ Esercitazione1/ConnectedModeClient.cs
@@ -0,0 +1,109 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+
+public static class ConnectedModeClient
+{
+    public static string ConnectionString { get; set; }
+    public static void ListTickets()
+    {
+        using SqlConnection connection = new SqlConnection(ConnectionString);
+        try
+        {
+            connection.Open();
+            SqlCommand selectCommand = connection.CreateCommand();
+            selectCommand.CommandType = System.Data.CommandType.Text;
+            selectCommand.CommandText = "SELECT * FROM Ticket ORDER BY Data DESC";
+
+            SqlDataReader reader = selectCommand.ExecuteReader();
+            Console.Clear();
+            Console.WriteLine("------Elenco Tickets------\n");
+            Console.WriteLine("ID\tDescrizione\tStato\t Creato\n");
+            Console.WriteLine("-------------------------");
+            while (reader.Read())
+            {
+                string formattedDate = ((DateTime)reader["Data"]).ToString("dd-MM-yyyy");
+                Console.WriteLine(reader["Id"].ToString() + "\t" + reader["Descrizione"].ToString() + "\t" +reader["Stato"].ToString() + "\t" +formattedDate);
+            }
+            Console.WriteLine("------------------------------");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            connection.Close();
+            Console.WriteLine("-----Premi un tasto------ ");
+            Console.ReadKey();
+        }
+    }
+
+    
+    public static void AddTicket()
+    {
+        using SqlConnection connection = new SqlConnection(ConnectionString);
+        try
+        {
+            connection.Open();
+            SqlCommand insertCommand= connection.CreateCommand();
+            insertCommand.CommandType = System.Data.CommandType.Text;
+            insertCommand.CommandText = "INSERT INTO Ticket VALUES(@descrizione, @data, @utente, @stato)";
+            Console.Clear();
+            Console.WriteLine("-------------Inserire un nuovo ticket-----------");
+            Console.Write("Descrizione: ");
+            string descrizione = Console.ReadLine();
+            insertCommand.Parameters.AddWithValue("@descrizione", descrizione);
+            insertCommand.Parameters.AddWithValue("@data", DateTime.Now);
+            Console.Write("Utente: ");
+            string utente=Console.ReadLine();
+            insertCommand.Parameters.AddWithValue("@utente", utente);
+            insertCommand.Parameters.AddWithValue("@stato", "New");
+
+            int result= insertCommand.ExecuteNonQuery();
+            if (result != 1)
+                Console.WriteLine("Si è verificato un problema nell'inserimento del ticket");
+            else
+                Console.WriteLine("Ticket aggiuto correttamente");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            connection.Close();
+            Console.WriteLine("-----Premi un tasto------ ");
+            Console.ReadKey();
+        }
+    }
+
+    public static void DeleteTicket()
+    {
+        using SqlConnection connection =new SqlConnection(ConnectionString);
+        try
+        {
+            connection.Open();
+            SqlCommand deleteCommand= connection.CreateCommand();
+            deleteCommand.CommandType = System.Data.CommandType.Text;
+            deleteCommand.CommandText = "DELETE from Ticket where id= @id";
+            Console.Write("Id del ticket da cancellare: ");
+            string idValue=Console.ReadLine();
+            deleteCommand.Parameters.AddWithValue("@id", idValue);
+            int result = deleteCommand.ExecuteNonQuery();
+            if (result != 1)
+                Console.WriteLine("Si è verificato un problema nell'eliminazione del ticket");
+            else
+                Console.WriteLine("Ticket eliminato correttamente");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            connection.Close();
+            Console.WriteLine("-----Premi un tasto------ ");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Esercitazione1/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--------Esercitazione 1 --------");
+string connectionStringSQL = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Ticketing;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+
+ConnectedModeClient.ConnectionString = connectionStringSQL;
+bool quit = false;
+do
+{
+    Console.WriteLine($"===========MENU==========\n");
+    Console.WriteLine("[1] - Elenco Tickets\n");
+    Console.WriteLine("[2] - Aggiungi Ticket\n");
+    Console.WriteLine("[3] - Elimina Ticket\n");
+    Console.WriteLine("[q] - QUIT\n");
+
+    string scelta = Console.ReadLine();
+    switch (scelta)
+    {
+        case "1":
+            //lista di tickets
+            ConnectedModeClient.ListTickets();
+            break;
+        case "2":
+            //aggiungi ticket
+            ConnectedModeClient.AddTicket();
+            break;
+        case "3":
+            //elimina ticket
+            ConnectedModeClient.DeleteTicket();
+            break;
+        case "q":
+            quit = true;
+            break;
+        default:
+            Console.WriteLine("Comando sconosciuto");
+            break;
+
+    }
+} while (!quit);
